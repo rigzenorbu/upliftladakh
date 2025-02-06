b149bda8a14da5dcaf430627ad11325a34d1eee9
Rigzen Norbu <rigzen077@gmail.com>
2025-02-06 18:40:36 +0530

    the scholarship, events and vision part added

--- src/script.js
+++ src/script.js
@@ -2,3 +2,24 @@ function showAbout() {
     const aboutSection = document.getElementById('about-section');
     aboutSection.classList.toggle('hidden'); // Toggles the "hidden" class
 }
+
+
+
+
+
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    navLinks.forEach(navLink => {
+      navLink.addEventListener('click', (e) => {
+        if (e.target.getAttribute('href') === '#scholarship') {
+          e.preventDefault();
+          document.querySelector('#scholarship').scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
+  
